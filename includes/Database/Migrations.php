b7a92d9acd0ea50de2eb6799cea350d0376967f2
Poserinka <info@poserinka.com>
2026-09-18 15:13:20 +0200

    Use red border for selected concert choice

--- includes/Database/Migrations.php
+++ includes/Database/Migrations.php
@@ -8,7 +8,7 @@ defined('ABSPATH') || exit;
 
 final class Migrations
 {
-    private const VERSION = '3.11.7';
+    private const VERSION = '3.11.8';
 
     public static function run(): void
     {
