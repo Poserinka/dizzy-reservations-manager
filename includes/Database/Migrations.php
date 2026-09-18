52d796782613fca402d780b2a4d743b772b148d6
Poserinka <info@poserinka.com>
2026-09-18 13:59:47 +0200

    Match concert choices to form label typography

--- includes/Database/Migrations.php
+++ includes/Database/Migrations.php
@@ -8,7 +8,7 @@ defined('ABSPATH') || exit;
 
 final class Migrations
 {
-    private const VERSION = '3.11.5';
+    private const VERSION = '3.11.6';
 
     public static function run(): void
     {
