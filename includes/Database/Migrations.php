3859e8761e4df46261506c0f577cfd3f248cc740
Poserinka <info@poserinka.com>
2026-09-18 13:51:48 +0200

    Restyle concert reservation choices

--- includes/Database/Migrations.php
+++ includes/Database/Migrations.php
@@ -8,7 +8,7 @@ defined('ABSPATH') || exit;
 
 final class Migrations
 {
-    private const VERSION = '3.11.4';
+    private const VERSION = '3.11.5';
 
     public static function run(): void
     {
