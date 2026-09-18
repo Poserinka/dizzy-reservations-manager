13ad2f8cbb54eb1538c6ab70bd5eed757e83ac7d
Poserinka <info@poserinka.com>
2026-09-18 13:34:58 +0200

    Make reservation message optional

--- includes/Database/Migrations.php
+++ includes/Database/Migrations.php
@@ -8,7 +8,7 @@ defined('ABSPATH') || exit;
 
 final class Migrations
 {
-    private const VERSION = '3.11.3';
+    private const VERSION = '3.11.4';
 
     public static function run(): void
     {
