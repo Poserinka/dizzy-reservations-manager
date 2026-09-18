bec78ac738edeb54e03d879845039514d6f73557
Poserinka <info@poserinka.com>
2026-09-18 15:45:29 +0200

    Style payment hold note as slot summary

--- includes/Database/Migrations.php
+++ includes/Database/Migrations.php
@@ -8,7 +8,7 @@ defined('ABSPATH') || exit;
 
 final class Migrations
 {
-    private const VERSION = '3.11.8';
+    private const VERSION = '3.11.9';
 
     public static function run(): void
     {
