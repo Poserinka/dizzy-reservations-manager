cc4f1bb69bf7ac93e8a6c4f31de95c40f98c9d18
Poserinka <info@poserinka.com>
2026-09-18 16:40:39 +0200

    Use theme outline style for send button

--- includes/Database/Migrations.php
+++ includes/Database/Migrations.php
@@ -8,7 +8,7 @@ defined('ABSPATH') || exit;
 
 final class Migrations
 {
-    private const VERSION = '3.11.12';
+    private const VERSION = '3.11.13';
 
     public static function run(): void
     {
